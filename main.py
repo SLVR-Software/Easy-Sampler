d4518ae729b56c395e1747e1903f483dc2db056c
Chris Massie <chris.l.massie@gmail.com>
2021-07-19 20:00:10 -0500

    :art: removed function code from this file and imported it instead

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
 from pytube import YouTube, Playlist
 # misc
 import os, sys
+#functions
+import downloadVideo
 
 FILE_PATH = r"C:\Users\Chris Massie\Google Drive\AudioVideoFiles"
-print(FILE_PATH)
+
+FILE_PATH = input("Please paste the file path save location:")
 
 FILENAME = "links.txt"
 
-
-isPlaylist = "False"
+functionNumber = "0"
 isAcceptableInput = False
 
+
 while isAcceptableInput == False:
-   print("True = using playlist")
-   print("False = using link file")
-   isPlaylist = (input("Please type True or False:"))
-   if (isPlaylist == "True" or isPlaylist == "False"):
+   print("0 - using playlist")
+   print("1 - using link file")
+   print("2 - using single video link")
+   functionNumber = (input("Please type True or False:"))
+   if (functionNumber == "0" or functionNumber == "1" or functionNumber == "2"):
       isAcceptableInput = True
       break
    else:
       print("Couldn't be bothered to bullet proof this, try again with 'True' or 'False'")
 
-if (isPlaylist == "False"):
+if (functionNumber == "0"):
    with open(FILENAME) as f:
       URLS = f.read().splitlines()
-elif (isPlaylist == "True"):
+      for url in URLS:
+         downloadVideo(url, FILE_PATH)
+elif (functionNumber == "1"):
    PLAYLIST_URL = input("Please paste the playlist url:")
    playlist = Playlist(PLAYLIST_URL)
    print(playlist.video_urls)
    URLS = playlist.video_urls
+   for url in URLS:
+      downloadVideo(url, FILE_PATH)
+elif (functionNumber == "2"):
+   URL = input("Paste the video link:")
+   downloadVideo(URL, FILE_PATH)
 
-for url in URLS:
-   video = YouTube(url)
-   videoTitle = (video.title).replace(":","")
-   STREAMS = video.streams
-   SAVE_PATH = os.path.join(FILE_PATH, (video.title).replace(":","").replace("'",""))
-   print(url)
-   for stream in STREAMS:
-      print("__________________________")
-      print("Downloading... " + video.title)
-      print("Details:")
-      print(stream)
-      isDownloaded = False
-      if (stream.type == "video"):
-         video.streams.get_by_itag(stream.itag).download(output_path=SAVE_PATH,filename_prefix=(stream.resolution+"-"+stream.type+"_"))
-      elif (stream.type == "audio"):
-         video.streams.get_by_itag(stream.itag).download(output_path=SAVE_PATH,filename_prefix=(stream.type+"_"))
+
+
+
+
+
+
+
 
 #TODO Only get one copy of the highest quality video
 #TODO Only get one copy of the highest quality video
