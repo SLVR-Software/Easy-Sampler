6829fa294b04628d4e9addc5fff6eddc57ea799b
Chris Massie <chris.l.massie@gmail.com>
2021-11-09 15:25:04 -0600

    :heavy_plus_sign:Added pyinstaller library

--- InstallLibraries.sh
+++ InstallLibraries.sh
@@ -3,9 +3,11 @@ pip install ffmpeg-python
 pip install moviepy
 pip install promptlib
 pip install dearpygui
+pip install pyinstaller
 
 pip install --upgrade pytube
 pip install --upgrade ffmpeg-python
 pip install --upgrade moviepy
 pip install --upgrade promptlib
 pip install --upgrade dearpygui
+pip install --upgrade pyinstaller
