ff9057462d5f7eca3f8a72beec1ad88037a47421
Chris Massie <chris.l.massie@gmail.com>
2021-11-09 13:54:46 -0600

    Added upgrade statements for all Libraries, can run this if you haven't used the project in a while and want to make sure you have all updates

--- InstallLibraries.sh
+++ InstallLibraries.sh
@@ -3,3 +3,9 @@ pip install ffmpeg-python
 pip install moviepy
 pip install promptlib
 pip install dearpygui
+
+pip install --upgrade pytube
+pip install --upgrade ffmpeg-python
+pip install --upgrade moviepy
+pip install --upgrade promptlib
+pip install --upgrade dearpygui
