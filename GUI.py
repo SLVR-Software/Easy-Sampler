67c0ff7fc73e3dab9a69e9aa4f2bb1e4e6474983
Chris Massie <chris.l.massie@gmail.com>
2021-11-11 00:16:54 -0600

    :hammer:Refactored GUI into tKinter library. Will be going with this for initial release at least

--- GUI.py
+++ GUI.py
@@ -1,55 +1,65 @@
-import dearpygui.dearpygui as dpg
-import helper
-
-dpg.create_context()
-dpg.create_viewport(title='Easy Sampler', width=600, height=200)
-
-def inputUrl_callback(sender, app_data, user_data):
-    print(sender)
-    url = helper.promptUserForDirectory()
-    print(url)
-    return url
-
-def samplePlaylist_callback(sender, app_data, user_data):
-    print(sender)
-    with dpg.window(label="Playlist Sampler"):
-        dpg.add_button(label="Pick Output Folder",callback=inputUrl_callback)
-    
-def youtubeSampler_callback(sender, app_data, user_data):
-    with dpg.window(label="Youtube Sampler", width=300, height=300):
-        dpg.add_text("Simply Sample")
-        dpg.add_button(label="Sample Youtube Video")
-        dpg.add_button(label="Sample Youtube Playlist",callback=samplePlaylist_callback)
-    
-
-with dpg.viewport_menu_bar():
-    with dpg.menu(label="File"):
-        dpg.add_menu_item(label="Save")
-        dpg.add_menu_item(label="Save As")
-
-        with dpg.menu(label="Settings"):
-            dpg.add_menu_item(label="Setting 1")
-            dpg.add_menu_item(label="Setting 2")
-
-    with dpg.menu(label="Open"):
-        dpg.add_menu_item(label="Youtube Sampler", callback=youtubeSampler_callback)
-
-    dpg.add_menu_item(label="Help")
-
-    
+from tkinter import *
+from tkinter import filedialog
 
 
-    with dpg.menu(label="Widget Items"):
-        dpg.add_checkbox(label="Pick Me")
-        dpg.add_button(label="Press Me")
-        dpg.add_color_picker(label="Color Me")
+root = Tk()
+
+root.title("Easy Sampler")
+root.iconbitmap('img/record.ico')
+
+def on_focusIn(event):
+    YOUTUBE_URL.config(foreground='black')
+    if YOUTUBE_URL.get() == "https://www.youtube.com/watch?v=00000000000":
+        event.widget.delete(0, END)
+    else:
+        YOUTUBE_URL.config(foreground='black')
+
+def on_focusOut(event):
+    if YOUTUBE_URL.get().replace(" ","") == "":
+        default_YOUTUBE_URL_TEXT.set("https://www.youtube.com/watch?v=00000000000")
+        YOUTUBE_URL.config(foreground='gray')
+    else:
+        userText = YOUTUBE_URL.get().replace(" ","")
+        event.widget.delete(0, END)
+        default_YOUTUBE_URL_TEXT.set(userText)
+
+
+def clickSample():
+    #PLAYLIST.processPlaylist(root.directory,False)
+    pass
+
+def clickDirSelector():
+    root.directory = filedialog.askdirectory()
+    print(root.directory)
+    dirLabel['text'] = root.directory
+
+default_YOUTUBE_URL_TEXT = StringVar()
+default_YOUTUBE_URL_TEXT.set("https://www.youtube.com/watch?v=00000000000")
+
+YOUTUBE_URL = Entry(root, width=50, textvariable=default_YOUTUBE_URL_TEXT)
+YOUTUBE_URL.bind("<FocusIn>", on_focusIn)
+YOUTUBE_URL.bind("<FocusOut>", on_focusOut)
+YOUTUBE_URL.grid(row=0, column=0)
+YOUTUBE_URL.config(foreground='gray')
+#YOUTUBE_URL.insert(0, "Enter the Youtube URL")
+
+dirSelectButton = Button(root, text="Folder", command=clickDirSelector)
+dirSelectButton.grid(row=1,column=0)
+
+dirLabel = Label(text="")
+
+dirLabel.grid(row=2,column=0)
+
+sampleButton = Button(root, text="Sampling", command=clickSample)
+sampleButton.grid(row=3,column=0)
 
 
 
-dpg.set_viewport_small_icon("./img/record.ico")
-dpg.set_viewport_large_icon("./img/record.ico")
 
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+
+root.mainloop()
+
+if __name__ == "__main__":
+   print("File one executed when ran directly")
+else:
+   print("File one executed when imported")
