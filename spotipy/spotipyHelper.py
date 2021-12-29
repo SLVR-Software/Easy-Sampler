26d8e3cb96b3012bdaa7ae1cecf479e614a019dc
Chris Massie <chris.l.massie@gmail.com>
2021-12-28 21:56:28 -0600

    :sparkles:was able to return some basic information from spotify

--- spotipy/spotipyHelper.py
+++ spotipy/spotipyHelper.py
@@ -1,24 +1,12 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+import cred
 
-cid = '84ef05fc5f084a8cb0a368d030c2f3d4'
-secret = '63938954d0c04976894cabc84eb381f9'
+scope = "user-read-recently-played"
 
-client_credentials_manager = SpotifyClientCredentials(client_id=cid,
-client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_id, client_secret= cred.client_secret, redirect_uri=cred.redirect_url, scope=scope))
 
-artist_name = []
-track_name = []
-popularity = []
-track_id = []
-for i in range(0,10000,50):
-    track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
-    for i, t in enumerate(track_results['tracks']['items']):
-        artist_name.append(t['artists'][0]['name'])
-        track_name.append(t['name'])
-        track_id.append(t['id'])
-        popularity.append(t['popularity'])
-
-print(artist_name)
-print(popularity)
+results = sp.current_user_recently_played()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
